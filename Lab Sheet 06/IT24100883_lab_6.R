d80fbb18b0281ebc3ba05a86f8e795e4085f9500
it24100883 <it24100883@my.sliit.lk>
2025-09-15 15:15:47 +0530

    Add Lab_06

---
+++ Lab Sheet 06/IT24100883_lab_6.R
@@ -0,0 +1,16 @@
+setwd("D:\\SLIIT\\Ps_Lab_06")
+##Q1
+#1.
+#Binomial distribution
+#random variable x has binomial distribution with n=50,p=0.85
+
+#2
+dbinom(47,50,0.85)
+
+##Q2
+#1 Number  of calls received per hour
+
+#2 Poisson distribution
+
+#3
+dpois(15,12)
